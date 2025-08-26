34386dde69a382d16ab16662c448e7421f9d5900
Kingkongkingston <akronnie55@gmail.com>
2025-08-26 10:01:43 -0500

    08/26/2025

--- week1/howdy_world.py
+++ week1/howdy_world.py
@@ -1,2 +1,11 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Kingston Ronnie Alexander
+# Section: 506
+# Assignment: Lab 1.11 - Howdy World
+# Date: 26 August 2025
+
 print("Howdy, World!")
 print("I code in Python for fun.")

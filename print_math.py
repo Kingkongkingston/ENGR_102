4fa6368220c8ef737f8af4c25f5347b28a93303b
Kingkongkingston <akronnie55@gmail.com>
2025-08-26 10:29:43 -0500

    08/26/2025

--- print_math.py
+++ print_math.py
@@ -12,14 +12,7 @@
 import math
 from math import sin, pi
 
-reynoldsNumber = (0.875*9)/(0.0015)
-productionRate = (100)/(1 + 0.8*2*10)**(1/0.8)
-# Convert the degrees to radians for sin or trig functions in general
-wavelength = (2*0.029*sin(35*pi/180))/(1)
-# Use math.log for natural logarithm
-changeInVelocity = (2029)*math.log(11000/8300)
-
-print(f"Reynolds number is {reynoldsNumber}")
-print(f"Wavelength is {wavelength} nm")
-print(f"Production rate is {productionRate} barrels/day")
-print(f"Change in velocity is {changeInVelocity} m/s")
+print("Reynolds number is", (0.875*9)/(0.0015))
+print("Wavelength is", (100)/(1 + 0.8*2*10)**(1/0.8), "nm")
+print("Production rate is", (2*0.029*sin(35*pi/180))/(1), "barrels/day")
+print("Change in velocity is", (2029)*math.log(11000/8300), "m/s")

75fe5b9138fc6fc38efab23bebb3e875c67f275d
Kingkongkingston <akronnie55@gmail.com>
2025-08-30 18:50:55 -0500

    08/30/2025

---
+++ week2/python_code_soup.py
@@ -0,0 +1,173 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kingston Alexander
+# Section:      509
+# Assignment:   2.11 Lab - Python Code Soup
+# Date:         30 August 2025
+
+# Calculations for 1
+x = 1
+z = 0
+z += x
+print(z)
+
+# Calculations for 29
+x = 1
+z = 0
+y = 10
+z += y
+z += y
+z += y
+z -= x
+print(z)
+
+# Calculations for 102
+x = 1
+y = 10
+z = 0
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += x
+z += x
+print(z)
+
+# Calculations for 100,000,000,000,000
+x = 1
+y = 10
+z = 0
+x = y
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+z += y
+print(z)
+
+# Calculations for 8675
+x = 1
+y = 10
+z = 0
+x = y
+y *= x
+# y = 100
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+x = 1
+y = 10
+# z = 8600
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+z += y
+x += 1
+x += 1
+x += 1
+x += 1
+z += x
+print(z)
